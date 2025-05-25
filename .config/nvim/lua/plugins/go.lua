622d6326f97dec9bef403a92eef2b98e1288463b
Suguru Akiho <s-akiho@unext.jp>
2025-05-25 16:29:07 +0900

    upgrade golang settings

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -1,74 +1,179 @@
 return {
-  -- vim-go
+  -- メインのGo開発環境
   {
-    'fatih/vim-go',
-    ft = 'go',
+    'ray-x/go.nvim',
+    dependencies = {
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    ft = {'go', 'gomod', 'gowork', 'gotmpl'},
     config = function()
-      -- vim-goの基本設定
-      vim.g.go_highlight_functions = 1
-      vim.g.go_highlight_methods = 1
-      vim.g.go_highlight_structs = 1
-      vim.g.go_highlight_interfaces = 1
-      vim.g.go_highlight_operators = 1
-      vim.g.go_highlight_build_constraints = 1
-      vim.g.go_highlight_types = 1
-      vim.g.go_highlight_extra_types = 1
-      vim.g.go_highlight_generate_tags = 1
-      vim.g.go_highlight_fields = 1
+      require('go').setup({
+        -- gopls の設定
+        lsp_cfg = {
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+                shadow = true,
+              },
+              staticcheck = true,
+              gofumpt = true,
+            },
+          },
+        },
+        -- コード診断の設定
+        diagnostic = {
+          enable = true,
+          virtual_text = true,
+        },
+        -- ハイライト設定
+        highlight = {
+          enable = true,
+          operators = true,
+          functions = true,
+          variables = true,
+          types = true,
+          fields = true,
+        },
+        -- テスト設定
+        test_runner = 'go',
+        run_in_floaterm = true,
+        -- タグの自動生成設定
+        tag_transform = false,
+        -- デバッグ設定
+        dap_debug = true,
+        -- lsp設定
+        lsp_document_formatting = true,
+        -- imports設定
+        lsp_inlay_hints = {
+          enable = true,
+          show_parameter_hints = true,
+          parameter_hints_prefix = "",
+          other_hints_prefix = "",
+        },
+      })
 
-      -- GoMetaLinter設定
-      vim.g.go_metalinter_autosave = 1
-      vim.g.go_metalinter_command = "golangci-lint"
-      vim.g.go_fmt_command = "goimports"
-
-      -- キーマッピング
+      -- シンタックスハイライトの設定
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "go",
         callback = function()
-          -- 基本的なキーマッピング
-          local opts = { buffer = true }
-          vim.keymap.set('n', 'gb', '<cmd>GoBuild<CR>', opts)
-          vim.keymap.set('n', 'gbb', '<cmd>GoTestFunc<CR>', opts)
-          vim.keymap.set('n', 'gl', '<cmd>GoLint<CR>', opts)
-          vim.keymap.set('n', 'gr', '<cmd>GoReferrers<CR>', opts)
-          vim.keymap.set('n', 'gf', '<cmd>GoIfErr<CR>', opts)
-          vim.keymap.set('n', 'gh', '<cmd>GoSameIds<CR>', opts)
-          vim.keymap.set('n', 'gi', '<cmd>GoInfo<CR>', opts)
-          vim.keymap.set('n', 'gd', '<cmd>GoDoc<CR>', opts)
-          vim.keymap.set('n', 'gc', '<cmd>GoCoverageToggle<CR>', opts)
-          vim.keymap.set('n', 'ga', '<cmd>GoAddTags<CR>', opts)
-          vim.keymap.set('n', 'gn', '<cmd>GoRename<CR>', opts)
+          -- 基本的なハイライト
+          vim.opt_local.syntax = "on"
+          
+          -- Goのハイライト設定
+          local highlight_groups = {
+            goField = { fg = "#66D9EF" },           -- フィールド
+            goFunction = { fg = "#A6E22E" },        -- 関数
+            goFunctionCall = { fg = "#A6E22E" },    -- 関数呼び出し
+            goVarDefs = { fg = "#FD971F" },         -- 変数定義
+            goVarAssign = { fg = "#FD971F" },       -- 変数代入
+            goVar = { fg = "#FD971F" },             -- 変数
+            goConst = { fg = "#AE81FF" },           -- 定数
+            goType = { fg = "#66D9EF" },            -- 型
+            goTypeName = { fg = "#66D9EF" },        -- 型名
+            goBuiltins = { fg = "#AE81FF" },        -- ビルトイン
+            goPackage = { fg = "#F92672" },         -- パッケージ
+            goImport = { fg = "#F92672" },          -- インポート
+            goComment = { fg = "#75715E" },         -- コメント
+            goString = { fg = "#E6DB74" },          -- 文字列
+          }
 
-          -- Leader キーマッピング
-          vim.keymap.set('n', '<Leader>r', '<Plug>(go-run)', opts)
-          vim.keymap.set('n', '<Leader>b', '<Plug>(go-build)', opts)
-          vim.keymap.set('n', '<Leader>t', '<Plug>(go-test)', opts)
-          vim.keymap.set('n', '<Leader>c', '<Plug>(go-coverage)', opts)
-          vim.keymap.set('n', '<Leader>ds', '<Plug>(go-def-split)', opts)
-          vim.keymap.set('n', '<Leader>dv', '<Plug>(go-def-vertical)', opts)
-          vim.keymap.set('n', '<Leader>dt', '<Plug>(go-def-tab)', opts)
-          vim.keymap.set('n', '<Leader>gd', '<Plug>(go-doc)', opts)
-          vim.keymap.set('n', '<Leader>gv', '<Plug>(go-doc-vertical)', opts)
-          vim.keymap.set('n', '<Leader>gb', '<Plug>(go-doc-browser)', opts)
-          vim.keymap.set('n', '<Leader>s', '<Plug>(go-implements)', opts)
-          vim.keymap.set('n', '<Leader>i', '<Plug>(go-info)', opts)
-          vim.keymap.set('n', '<Leader>e', '<Plug>(go-rename)', opts)
-
+          -- ハイライトグループの適用
+          for group, colors in pairs(highlight_groups) do
+            vim.api.nvim_set_hl(0, group, colors)
+          end
+          
           -- エラーハイライト
-          vim.cmd [[highlight goErr cterm=bold ctermfg=214]]
-          vim.cmd [[match goErr /\<err\>/]]
+          vim.api.nvim_set_hl(0, 'goErr', { fg = '#FF0000', bold = true })
+          vim.fn.matchadd('goErr', '\\<err\\>')
         end
       })
-    end
-  },
-  
-  -- gitgutter
-  {
-    'airblade/vim-gitgutter',
-    config = function()
+
       -- キーマッピング
-      vim.keymap.set('n', ',gg', '<cmd>GitGutterToggle<CR>', { silent = true })
-      vim.keymap.set('n', ',gh', '<cmd>GitGutterLineHighlightsToggle<CR>', { silent = true })
-    end
-  }
+      local opts = { noremap = true, silent = true }
+      -- 基本的な機能
+      vim.keymap.set('n', '<leader>gt', '<cmd>GoTest<CR>', opts)
+      vim.keymap.set('n', '<leader>gr', '<cmd>GoRun<CR>', opts)
+      vim.keymap.set('n', '<leader>gb', '<cmd>GoBuild<CR>', opts)
+      vim.keymap.set('n', '<leader>gf', '<cmd>GoFillStruct<CR>', opts)
+      vim.keymap.set('n', '<leader>gi', '<cmd>GoImport<CR>', opts)
+      
+      -- デバッグ関連
+      vim.keymap.set('n', '<leader>gdb', '<cmd>GoDebug<CR>', opts)
+      vim.keymap.set('n', '<leader>gdt', '<cmd>GoDebugTest<CR>', opts)
+      vim.keymap.set('n', '<leader>gdr', '<cmd>GoDebugRestart<CR>', opts)
+      
+      -- コード解析・リファクタリング
+      vim.keymap.set('n', '<leader>gc', '<cmd>GoCoverage<CR>', opts)
+      vim.keymap.set('n', '<leader>gl', '<cmd>GoLint<CR>', opts)
+      vim.keymap.set('n', '<leader>ga', '<cmd>GoAlt<CR>', opts)
+      vim.keymap.set('n', '<leader>gm', '<cmd>GoModTidy<CR>', opts)
+    end,
+  },
+
+  -- TreeSitter設定
+  {
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'go', 'gomod', 'gowork' },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+
+  -- デバッグ用のプラグイン
+  {
+    'leoluz/nvim-dap-go',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'rcarriga/nvim-dap-ui',
+    },
+    config = function()
+      require('dap-go').setup({
+        -- delve の設定
+        delve = {
+          initialize_timeout_sec = 20,
+          port = "${port}",
+        },
+      })
+    end,
+  },
+
+  -- コード補完の強化
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
+  },
+
+  -- スニペット
+  {
+    'L3MON4D3/LuaSnip',
+    config = function()
+      require('luasnip').add_snippets('go', {
+        -- Goのスニペット定義
+        require('luasnip').snippet(
+          { trig = "eif", name = "Error If" },
+          {
+            require('luasnip').text_node({"if err != nil {", "\treturn err", "}"})
+          }
+        ),
+        -- 他のスニペットもここに追加可能
+      })
+    end,
+  },
 }
